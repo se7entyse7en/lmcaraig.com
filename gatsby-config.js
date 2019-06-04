228e129661eacaa4d89158192d58abb326eeb165
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-06-04 11:30:11 +0200

    Add gatsby-config.js
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: `Lou Marvin Caraig - Blog`,
+    siteUrl: `http://lmcaraig.com`,
+    header: {
+      name: `Lou Marvin Caraig`,
+      position: `Software Development Engineer`,
+      company: `source{d}`,
+    }
+  },
+}
