0062b6a1c0786e1d18ad81801dae7b4ecbb52a64
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-06-10 19:39:56 +0200

    Make about load from markdown file
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,49 @@
 import React from "react"
-import Layout from "../components/layout"
 
-export default ({data}) => (
-  <Layout withNavbar={true}>
-    <div className="container">
-      <p>It's me!</p>
-    </div>
-  </Layout>
-)
+import { LayoutWithDefaultProps } from "../components/layout"
+
+export default ({data}) => {
+  const node = data.allMarkdownRemark.edges[0].node
+  return (
+    <LayoutWithDefaultProps withNavbar={true} withHeader={false}>
+
+      <div className="container">
+
+        <div className="row mb-3">
+          <div className="col"></div>
+          <div className="col-8 col-sm-8 text-center">
+            <h1>{node.frontmatter.title}</h1>
+          </div>
+          <div className="col"></div>
+        </div>
+
+        <div className="row">
+          <div className="col"></div>
+          <div className="col-8 col-sm-8">
+            <div dangerouslySetInnerHTML={{ __html: node.html }}></div>
+          </div>
+          <div className="col"></div>
+        </div>
+
+      </div>
+
+    </LayoutWithDefaultProps>
+  )
+}
+
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/about.md/"}}) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
