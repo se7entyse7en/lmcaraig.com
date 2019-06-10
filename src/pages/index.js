133cea549e8693cf956e6025f223398b7ffd1a2c
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-06-10 17:54:37 +0200

    Use layout in index
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import Navbar from "../components/navbar"
-import Header from "../components/header"
+import { LayoutWithDefaultProps } from "../components/layout"
 import Posts from "../components/posts"
 
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
-import picture from "../../content/assets/picture.jpeg"
-
 export default ({data}) => (
-  <React.Fragment>
-    <Navbar></Navbar>
-    <Header picture={picture}
-            name={data.site.siteMetadata.header.name}
-            position={data.site.siteMetadata.header.position}
-            company={data.site.siteMetadata.header.company}
-            icons={[
-              [fab.faTwitter, `https://twitter.com/lmcaraig`],
-              [fab.faLinkedin, `https://linkedin.com/in/lmcaraig`],
-              [fab.faGithub, `https://github.com/se7entyse7en`],
-              [fab.faStackOverflow, `https://stackoverflow.com/users/3276106/se7entyse7en`],
-              [faEnvelope, `mailto:loumarvincaraig@gmail.com`]
-            ]}>
-    </Header>
+  <LayoutWithDefaultProps withNavbar={true} withHeader={true}>
     <Posts></Posts>
-  </React.Fragment>
+  </LayoutWithDefaultProps>
 )
 
 export const query = graphql`
