21b601fad1266fb4cf4a8d7ae9caf95cd651a0eb
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-11-02 14:34:40 +0100

    Format code
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,28 +4,28 @@ import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "../components/navbar"
 import Header from "../components/header"
 
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 import picture from "../../content/assets/picture.jpeg"
 
-
-const Layout = (props) => (
+const Layout = props => (
   <React.Fragment>
     {props.withNavbar ? <Navbar></Navbar> : null}
     {props.withHeader ? (
-      <Header picture={props.header.picture}
-              name={props.header.name}
-              position={props.header.position}
-              company={props.header.company}
-              icons={props.header.icons}>
-      </Header>
+      <Header
+        picture={props.header.picture}
+        name={props.header.name}
+        position={props.header.position}
+        company={props.header.company}
+        icons={props.header.icons}
+      ></Header>
     ) : null}
     {props.children}
   </React.Fragment>
 )
 
-const LayoutWithDefaultProps = (props) => {
+const LayoutWithDefaultProps = props => {
   const data = useStaticQuery(
     graphql`
       query {
@@ -43,26 +43,29 @@ const LayoutWithDefaultProps = (props) => {
   )
 
   return (
-    <Layout withNavbar={props.withNavbar} withHeader={props.withHeader}
-            header={
-              {
-                "picture": picture,
-                "name": data.site.siteMetadata.header.name,
-                "position": data.site.siteMetadata.header.position,
-                "company": data.site.siteMetadata.header.company,
-                "icons": [
-                  [fab.faTwitter, `https://twitter.com/lmcaraig`],
-                  [fab.faLinkedin, `https://linkedin.com/in/lmcaraig`],
-                  [fab.faGithub, `https://github.com/se7entyse7en`],
-                  [fab.faStackOverflow, `https://stackoverflow.com/users/3276106/se7entyse7en`],
-                  [faEnvelope, `mailto:loumarvincaraig@gmail.com`]
-                ],
-              }
-            }>
+    <Layout
+      withNavbar={props.withNavbar}
+      withHeader={props.withHeader}
+      header={{
+        picture: picture,
+        name: data.site.siteMetadata.header.name,
+        position: data.site.siteMetadata.header.position,
+        company: data.site.siteMetadata.header.company,
+        icons: [
+          [fab.faTwitter, `https://twitter.com/lmcaraig`],
+          [fab.faLinkedin, `https://linkedin.com/in/lmcaraig`],
+          [fab.faGithub, `https://github.com/se7entyse7en`],
+          [
+            fab.faStackOverflow,
+            `https://stackoverflow.com/users/3276106/se7entyse7en`,
+          ],
+          [faEnvelope, `mailto:loumarvincaraig@gmail.com`],
+        ],
+      }}
+    >
       {props.children}
     </Layout>
   )
-
 }
 
 export default Layout
--- src/templates/post.js
+++ src/templates/post.js
@@ -2,41 +2,43 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import { LayoutWithDefaultProps } from "../components/layout"
-import Toc  from "../components/toc"
+import Toc from "../components/toc"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 // this ensures that the icon CSS is loaded immediately before attempting to render icons
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
+import { fab } from "@fortawesome/free-brands-svg-icons"
 
 const INTRO_BEGIN_MARKER = "<!--BEGIN INTRO-->"
 const INTRO_END_MARKER = "<!--END INTRO-->"
 
-export default ({data}) => {
-  const post = data.markdownRemark;
-  const readingTimeUnitString = post.timeToRead > 1? "mins" : "min";
+export default ({ data }) => {
+  const post = data.markdownRemark
+  const readingTimeUnitString = post.timeToRead > 1 ? "mins" : "min"
 
   const introHtml = post.html.substring(
     post.html.lastIndexOf(INTRO_BEGIN_MARKER) + INTRO_BEGIN_MARKER.length,
     post.html.lastIndexOf(INTRO_END_MARKER)
-  );
+  )
 
   const bodyHtml = post.html.substring(
-    post.html.lastIndexOf(INTRO_END_MARKER) + INTRO_END_MARKER.length)
+    post.html.lastIndexOf(INTRO_END_MARKER) + INTRO_END_MARKER.length
+  )
 
   return (
     <LayoutWithDefaultProps withNavbar={true} withHeader={true}>
-
       <div className="container">
-
         <div className="row mb-3">
           <div className="col"></div>
           <div className="col-10 col-sm-8 text-center">
-            <p className="card-subtitle text-secondary">{post.frontmatter.formattedDate}</p>
+            <p className="card-subtitle text-secondary">
+              {post.frontmatter.formattedDate}
+            </p>
             <h1>{post.frontmatter.title}</h1>
-            <p className="text-secondary">~ {post.timeToRead} {readingTimeUnitString} read</p>
+            <p className="text-secondary">
+              ~ {post.timeToRead} {readingTimeUnitString} read
+            </p>
           </div>
           <div className="col"></div>
         </div>
@@ -68,15 +70,26 @@ export default ({data}) => {
         <div className="row mb-3 mt-3 text-right">
           <div className="col"></div>
           <div className="col-10 col-sm-8">
-            <a className="text-dark" href={"https://news.ycombinator.com/item?id=" + post.frontmatter.hackerNewsId}>
-              <h6><FontAwesomeIcon className="mx-1" icon={fab.faHackerNewsSquare} size="lg" /><i>See comments on HackerNews</i></h6>
+            <a
+              className="text-dark"
+              href={
+                "https://news.ycombinator.com/item?id=" +
+                post.frontmatter.hackerNewsId
+              }
+            >
+              <h6>
+                <FontAwesomeIcon
+                  className="mx-1"
+                  icon={fab.faHackerNewsSquare}
+                  size="lg"
+                />
+                <i>See comments on HackerNews</i>
+              </h6>
             </a>
           </div>
           <div className="col"></div>
         </div>
-
       </div>
-
     </LayoutWithDefaultProps>
   )
 }
